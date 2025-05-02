eee42111c521bdf28c73a6cf64223704668c66b8
Dan Hulmston <dan.hulmston_PA@hra.nhs.uk>
2025-05-02 11:52:19 +0100

    Added GDS Radio Button Group Tag Helper

---
+++ src/Rsp.Gds.Component/Models/GdsRadioOption.cs
@@ -0,0 +1,7 @@
+﻿namespace Rsp.Gds.Component.Models;
+
+public class GdsRadioOption
+{
+    public string Value { get; set; }
+    public string Label { get; set; }
+}
---
+++ src/Rsp.Gds.Component/TagHelpers/Base/RspGdsRadioGroupTagHelper.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Rsp.Gds.Component.TagHelpers.Base;
+
+[HtmlTargetElement("rsp-gds-radio-group", Attributes = ForAttributeName)]
+public class RspGdsRadioGroupTagHelper : TagHelper
+{
+    private const string ForAttributeName = "asp-for";
+
+    [HtmlAttributeName(ForAttributeName)] public ModelExpression For { get; set; }
+
+    [HtmlAttributeName("label-text")] public string LabelText { get; set; }
+
+    [HtmlAttributeName("options")] public IEnumerable<GdsRadioOption> Options { get; set; }
+
+    [ViewContext] [HtmlAttributeNotBound] public ViewContext ViewContext { get; set; }
+
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        var propertyName = For.Name;
+
+        // ✅ Safely extract selected value even if model is a List<string>
+        var selectedValue = For.Model switch
+        {
+            string str => str,
+            IEnumerable<string> list => list.FirstOrDefault(),
+            _ => For.Model?.ToString()
+        };
+
+        // ✅ Error handling
+        ViewContext.ViewData.ModelState.TryGetValue(propertyName, out var modelStateEntry);
+        var hasError = modelStateEntry != null && modelStateEntry.Errors.Count > 0;
+
+        var errorHtml = hasError
+            ? $"<span class='govuk-error-message'>{modelStateEntry.Errors[0].ErrorMessage}</span>"
+            : "";
+
+        output.TagName = "div";
+        output.TagMode = TagMode.StartTagAndEndTag;
+        output.Attributes.SetAttribute("class", $"govuk-form-group {(hasError ? "govuk-form-group--error" : "")}");
+
+        // ✅ Render radio options
+        var radiosHtml = string.Join("\n", Options.Select(option =>
+        {
+            var inputId = $"{propertyName}_{option.Value.Replace(" ", "_")}";
+            var isChecked = string.Equals(
+                selectedValue?.Trim(),
+                option.Value?.Trim(),
+                StringComparison.OrdinalIgnoreCase
+            )
+                ? "checked=\"checked\""
+                : "";
+
+            return $@"
+                    <div class='govuk-radios__item'>
+                        <input class='govuk-radios__input'
+                               id='{inputId}'
+                               name='{propertyName}'
+                               type='radio'
+                               value='{option.Value}'
+                               {isChecked} />
+                        <label class='govuk-label govuk-radios__label' for='{inputId}'>
+                            {option.Label}
+                        </label>
+                    </div>";
+        }));
+
+        var fieldsetHtml = $@"
+                <fieldset class='govuk-fieldset'>
+                    <legend class='govuk-fieldset__legend govuk-fieldset__legend--m'>
+                        <label class='govuk-label govuk-label--s' for='{propertyName}'>{LabelText}</label>
+                    </legend>
+                    {errorHtml}
+                    <div class='govuk-radios' data-module='govuk-radios'>
+                        {radiosHtml}
+                    </div>
+                </fieldset>";
+
+        output.Content.SetHtmlContent(fieldsetHtml);
+    }
+}
